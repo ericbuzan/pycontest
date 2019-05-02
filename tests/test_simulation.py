51cfab5d0370a76412a9f3b573c6cee9b390c2d8
Eric Buzan <eric.buzan@gmail.com>
2019-05-02 11:50:44 -0400

    Update test_simulation.py

--- tests/test_simulation.py
+++ tests/test_simulation.py
@@ -34,16 +34,3 @@ def test_simulation_1(dt):
     assert vel[1][0] == 1
     # Y component should stay the same
     assert (vel[0][1], vel[1][1]) == (vel_0[0][1], vel_0[1][1])
-
-def test_simulation_2(dt):
-    # initial condition and simulation parameters
-    domain = ([-2, 12], [0, 3])
-    t_max = 6
-    dt = 0.5
-    loc_0 = np.array([[0, 1.5],[10, 1.5]])
-    vel_0 = np.array([[1, 0], [-1, 0]])
-    radius = 1
-    mass = [1, 1]
-
-    # running the simulation
-    loc, vel = sim.simulation(t_max, dt, mass, radius, loc_0, vel_0, domain)
